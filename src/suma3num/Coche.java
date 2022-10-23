22afd92ea5afe649a6de4ea092c92212468df254
YesidRomero <yesid.romero.mt@usa.edu.co>
2022-10-23 16:57:55 -0500

    Ejercicio 1: Suma de 3 numeros Ejercicio 2: Creacion Clase, objeto e incremental.

---
+++ src/suma3num/Coche.java
@@ -0,0 +1,21 @@
+
+package suma3num;
+
+
+// @author Yapup //
+ 
+import java.util.Scanner;
+
+public class Coche {
+    
+    public static void main (String[] args) {
+    
+        int puerta = 4;
+        Coche miCoche = new Coche();
+        puerta++;
+    
+    System.out.println(puerta);
+
+    }
+    
+}
---
+++ src/suma3num/Suma3Num.java
@@ -0,0 +1,18 @@
+
+package suma3num;
+
+// @author Yapup //
+ 
+public class Suma3Num {
+
+    public static void main(String[] args) {
+        int numero1 = 3;
+        int numero2 = 21;
+        int numero3 = 12;
+        
+        int suma = numero1 + numero2 + numero3;
+        
+        System.out.println("El resultado es: " + suma);
+    }
+    
+}
